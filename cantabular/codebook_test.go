da967910bc053949a0c98fffc7f8f26a18297b37
Joshua Medley <jmedleyx7@gmail.com>
2021-06-08 16:53:19 +0100

    Add happy path test for cantabular client

--- cantabular/codebook_test.go
+++ cantabular/codebook_test.go
@@ -2,6 +2,7 @@ package cantabular_test
 
 import (
 	"bytes"
+	"fmt"
 	"context"
 	"io/ioutil"
 	"net/http"
@@ -12,14 +13,14 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestGetCodebook(t *testing.T) {
+func TestGetCodebookUnhappy(t *testing.T) {
 
-	Convey("Given a 404 response from the /Codebook endpoint", t, func() {
+	Convey("Given a non-200 response from the /Codebook endpoint", t, func() {
 		testCtx := context.Background()
 
 		mockHttpClient := &dphttp.ClienterMock{
 			GetFunc: func(ctx context.Context, url string) (*http.Response, error) {
-				return Response(make([]byte, 0), 404), nil
+				return Response(make([]byte, 0), http.StatusNotFound), nil
 			},
 		}
 
@@ -40,6 +41,45 @@ func TestGetCodebook(t *testing.T) {
 	})
 }
 
+func TestGetCodebookHappy(t *testing.T) {
+
+	Convey("Given a correct response from the /Codebook endpoint", t, func() {
+		testCtx := context.Background()
+
+		resp, err := testCodebookResponse()
+		So(err, ShouldBeNil)
+
+		mockHttpClient := &dphttp.ClienterMock{
+			GetFunc: func(ctx context.Context, url string) (*http.Response, error) {
+				return Response(
+					resp,
+					http.StatusOK,
+				), nil
+			},
+		}
+
+		cantabularClient := cantabular.NewClient(
+			mockHttpClient,
+			cantabular.Config{},
+		)
+
+		Convey("When the GetCodebook method is called", func() {
+			req := cantabular.GetCodebookRequest{}
+			cb, err := cantabularClient.GetCodebook(testCtx, req)
+			So(err, ShouldBeNil)
+
+			Convey("Then the expected codebook information should be returned", func() {
+				So(cb.Dataset.Name,            ShouldEqual,      "Example")
+				So(cb.Codebook,                ShouldHaveLength,  5)
+				So(cb.Codebook[0].Name,        ShouldEqual,      "city")
+				So(cb.Codebook[1].Labels[0],   ShouldEqual,      "England")
+				So(cb.Codebook[3].Codes[2],    ShouldEqual,      "2")
+				So(cb.Codebook[4].Labels,      ShouldHaveLength,  3)
+			})
+		})
+	})
+}
+
 func Response(body []byte, statusCode int) *http.Response {
 	reader := bytes.NewBuffer(body)
 	readCloser := ioutil.NopCloser(reader)
@@ -48,4 +88,13 @@ func Response(body []byte, statusCode int) *http.Response {
 		StatusCode: statusCode,
 		Body:       readCloser,
 	}
+}
+
+func testCodebookResponse() ([]byte, error){
+	b, err := ioutil.ReadFile("codebook_test.json")
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %s", err)
+	}
+
+	return b, nil
 }
--- cantabular/contract.go
+++ cantabular/contract.go
@@ -5,12 +5,14 @@ type ErrorResponse struct{
 	Message string `json:"message"`
 }
 
+// GetCodebookRequest holds the query parameters for GET /codebook/{dataset}?cats=xxx&v=xxx
 type GetCodebookRequest struct{
 	DatasetName string 
 	Variables   []string
 	Categories  bool
 }
 
+// GetCodebookRequest holds the response body for GET /codebook/{dataset}?cats=xxx&v=xxx
 type GetCodebookResponse struct{
 	Codebook Codebook `json:"codebook"`
 	Dataset  Dataset  `json:"dataset"`
