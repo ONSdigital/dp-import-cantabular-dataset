e6b8dc6b2d2a7184331a49978e8acbc024e00c1a
Joshua Medley <jmedleyx7@gmail.com>
2021-06-16 23:32:40 +0100

    remove statuscode from handler error

--- handler/error.go
+++ handler/error.go
@@ -2,16 +2,11 @@ package handler
 
 type Error struct{
 	err error
-	statusCode int
 	logData map[string]interface{}
 }
 
 func (e *Error) Error() string{
 	return e.err.Error()
-}
-
-func (e *Error) Code() int{
-	return e.statusCode
 }
 
 func (e *Error) LogData() map[string]interface{}{
