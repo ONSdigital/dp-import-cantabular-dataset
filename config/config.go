785573a1c7170742cc000657c1d06846481c5825
Joshua Medley <jmedleyx7@gmail.com>
2021-06-16 23:11:59 +0100

    slightly less crazy variable name

--- config/config.go
+++ config/config.go
@@ -8,23 +8,23 @@ import (
 
 // Config represents service configuration for dp-import-cantabular-dataset
 type Config struct {
-	BindAddr                   string        `envconfig:"BIND_ADDR"`
-	GracefulShutdownTimeout    time.Duration `envconfig:"GRACEFUL_SHUTDOWN_TIMEOUT"`
-	HealthCheckInterval        time.Duration `envconfig:"HEALTHCHECK_INTERVAL"`
-	HealthCheckCriticalTimeout time.Duration `envconfig:"HEALTHCHECK_CRITICAL_TIMEOUT"`
-	KafkaAddr                  []string      `envconfig:"KAFKA_ADDR"                     json:"-"`
-	KafkaVersion               string        `envconfig:"KAFKA_VERSION"`
-	KafkaOffsetOldest          bool          `envconfig:"KAFKA_OFFSET_OLDEST"`
-	KafkaNumWorkers            int           `envconfig:"KAFKA_NUM_WORKERS"`
-	InstanceStartedGroup       string        `envconfig:"KAFKA_DATASET_INSTANCE_STARTED_GROUP"`
-	InstanceStartedTopic       string        `envconfig:"KAFKA_DATASET_INSTANCE_STARTED_TOPIC"`
-	CantabularDatasetCategoryDimensionImportTopic string        `envconfig:"CANTABULAR_DATASET_CATEGORY_DIMENSION_IMPORT"`
-	OutputFilePath             string        `envconfig:"OUTPUT_FILE_PATH"`
-	DatasetAPIURL              string        `envconfig:"DATASET_API_URL"`
-	CodelistAPIURL             string        `envconfig:"CODELIST_API_URL"`
-	RecipeAPIURL               string       ` envconfig:"RECIPE_API_URL"`
-	CantabularURL              string        `envconfig:"CANTABULAR_URL"`
-	ServiceAuthToken           string        `envconfig:"SERVICE_AUTH_TOKEN"         json:"-"`
+	BindAddr                     string        `envconfig:"BIND_ADDR"`
+	GracefulShutdownTimeout      time.Duration `envconfig:"GRACEFUL_SHUTDOWN_TIMEOUT"`
+	HealthCheckInterval          time.Duration `envconfig:"HEALTHCHECK_INTERVAL"`
+	HealthCheckCriticalTimeout   time.Duration `envconfig:"HEALTHCHECK_CRITICAL_TIMEOUT"`
+	KafkaAddr                    []string      `envconfig:"KAFKA_ADDR"                     json:"-"`
+	KafkaVersion                 string        `envconfig:"KAFKA_VERSION"`
+	KafkaOffsetOldest            bool          `envconfig:"KAFKA_OFFSET_OLDEST"`
+	KafkaNumWorkers              int           `envconfig:"KAFKA_NUM_WORKERS"`
+	InstanceStartedGroup         string        `envconfig:"KAFKA_DATASET_INSTANCE_STARTED_GROUP"`
+	InstanceStartedTopic         string        `envconfig:"KAFKA_DATASET_INSTANCE_STARTED_TOPIC"`
+	CategoryDimensionImportTopic string        `envconfig:"CANTABULAR_DATASET_CATEGORY_DIMENSION_IMPORT"`
+	OutputFilePath               string        `envconfig:"OUTPUT_FILE_PATH"`
+	DatasetAPIURL                string        `envconfig:"DATASET_API_URL"`
+	CodelistAPIURL               string        `envconfig:"CODELIST_API_URL"`
+	RecipeAPIURL                 string       ` envconfig:"RECIPE_API_URL"`
+	CantabularURL                string        `envconfig:"CANTABULAR_URL"`
+	ServiceAuthToken             string        `envconfig:"SERVICE_AUTH_TOKEN"         json:"-"`
 }
 
 var cfg *Config
@@ -54,7 +54,7 @@ func Get() (*Config, error) {
 		RecipeAPIURL:               "http://localhost:22300",
 		CantabularURL:              "http://localhost:8491",
 		ServiceAuthToken:           "",
-		CantabularDatasetCategoryDimensionImportTopic: "cantabular-dataset-category-dimension-import",
+		CategoryDimensionImportTopic: "cantabular-dataset-category-dimension-import",
 	}
 
 	return cfg, envconfig.Process("", cfg)
