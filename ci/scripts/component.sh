a655c26b11247341a6b12f6875435923390d99b6
David Subiros <subiros.david@gmail.com>
2021-12-08 11:01:17 +0000

    increased docker-compose timeout

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 pushd dp-import-cantabular-dataset
-  COMPONENT_TEST_USE_LOG_FILE=true make test-component
+  COMPOSE_HTTP_TIMEOUT=240 COMPONENT_TEST_USE_LOG_FILE=true make test-component
   e=$?
   f="log-output.txt"
   cat $f && rm $f
